f0c19a089682b808f332bb39df45f09f2a8d6ff4
riki-r <rikihavlin@gmail.com>
2017-12-25 18:46:10 +0200

    GREEN:pass(zero tests)

--- test/test.js
+++ test/test.js
@@ -3,8 +3,6 @@ var C = require('../cash.js');  // our module
 
 describe('Array', function(){
   describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(4)); // 4 is not present in this array so indexOf returns -1
-    })
+   
   })
 });
